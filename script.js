9d109f99397407a24b79f5753ace0c6a54403eeb
Hotgodess <152019838+Marcel18102@users.noreply.github.com>
2023-11-25 08:09:22 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Your JavaScript code goes here
+    // For example, let's add a simple alert
+    alert('Welcome to Your Website!');
+});
